50d064d82e44da20fe37b88c88898bd878dd8f49
Lucas Beserra <lucasbeserra932@gmail.com>
2023-01-16 14:49:32 -0300

    Optimized result component

--- src/components/BmiResult.jsx
+++ src/components/BmiResult.jsx
@@ -10,7 +10,9 @@ export default class BmiResult extends React.Component {
     render(){
         return(
             <div className="max-w-md flex flex-col items-center">
-                <p className="text-xl">Resultado: {this.props.result.toFixed(2)}</p>
+                <p className="text-xl">{this.props.result == 0 ? 
+                "Informe seu peso e altura nos campos acima" : 
+                `Resultado: ${this.props.result.toFixed(2)}`}</p>
                 <hr/>
                 <Tip result={this.props.result}/>
             </div>
